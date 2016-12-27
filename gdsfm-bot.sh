e2f55a4cea5905088c364f8eda7f1bad5440a9fa
Ueli Kunz <kunz@ideadapt.net>
2016-12-28 00:59:04 +0100

    sdlkfjsdf

--- gdsfm-bot.sh
+++ gdsfm-bot.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-node gdsfm-bot-master/gdsfm-bot/telebot.js &
+java -jar gdsfm-bot-master/gdsfm-server/target/telegram-bot-0.0.1-SNAPSHOT.jar &
 
-java -jar gdsfm-bot-master/gdsfm-server/target/telegram-bot-0.0.1-SNAPSHOT.jar
+node gdsfm-bot-master/gdsfm-bot/telebot.js
+
 
 
