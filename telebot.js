c013ed5fa6ddcba1e1bcdcc22b22e5181e0804cc
Ueli Kunz <kunz@ideadapt.net>
2016-12-27 16:22:57 +0100

    reinstall

--- telebot.js
+++ telebot.js
@@ -9,22 +9,19 @@ const bot = new TeleBot({
     limit: 100, // Optional. Limits the number of updates to be retrieved.
     retryTimeout: 5000 // Optional. Reconnecting timeout (in ms).
   }
-});
+})
 
-bot.on('text', msg => {
-  let fromId = msg.from.id;
-  let firstName = msg.from.first_name;
-  let reply = msg.message_id;
-  //return bot.sendMessage(fromId, `Welcome, ${ firstName }!`, { reply });
-  return request.get({url:'http://gdsfm.airtime.pro/api/item-history-feed'}, (err, res, body)=>{
-    let json = JSON.parse(body);
-    json.sort((a, b) => {
-        return new Date(a.starts).getTime() - new Date(b.ends).getTime();
-    });
-    console.log(json);
-    return bot.sendMessage(fromId, body.toString().substring(0, 10));    
-  });
-  
-});
+const apiRoot = 'http://gdsfm.airtime.pro/api/item-history-feed'
 
-bot.connect();
+bot.on('/history', msg => {
+  let [cmdName, nrOfItems] = msg.text.split(' ')
+  let chatId = msg.from.id
+  let reply = msg.message_id
+  console.log(cmdName, nrOfItems)
+  return request.get({url: apiRoot}, (err, res, body)=>{
+    let json = JSON.parse(body)
+    return bot.sendMessage(chatId, json.toString().substring(0, 10), {reply})    
+  })
+})
+
+bot.connect()
